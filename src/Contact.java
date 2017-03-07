08e5a00bc1f28e5ea48b330948cce48797895b8e
urosh t <nediarovic@gmail.com>
2017-03-07 07:49:52 +0100

    Optimized the code and made a few changes to the existing functions

--- src/Contact.java
+++ src/Contact.java
@@ -33,22 +33,4 @@ public class Contact {
         return this.email;
     }*/
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Contact contact = (Contact) o;
-
-        //if (number != contact.number) return false;
-        //return name.equals(contact.name);
-        return number == contact.number;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name.hashCode();
-        result = 31 * result + number;
-        return result;
-    }
 }
--- src/Phonebook.java
+++ src/Phonebook.java
@@ -10,45 +10,54 @@ import java.util.ArrayList;
 
 public class Phonebook {
 
+    static Scanner userWordInput = new Scanner(System.in);
+    static Scanner userNumInput = new Scanner (System.in);
+    ArrayList<String> menuOptions = new ArrayList<>();
     private ArrayList<Contact> contactList;
-    // ArrayList<String> menuOptions = new ArrayList<>();
-    static Scanner nameInput = new Scanner(System.in);
-    static Scanner numInput = new Scanner(System.in);
 
-    // why this doesn't work?
-    /*String option1 = " 1 - Add a contact"; menuOptions.add(option1);
+    String option1 = " 1 - Add a contact";
     String option2 = " 2 - Remove a contact";
     String option3 = " 3 - Find a contact";
     String option4 = " 4 - List all contacts";
-    String option5 = " 5 Dial a contact";*/
+    String option5 = " 5 - Dial a contact";
+    String option6 = " 6 - Exit the phonebook";
 
     public Phonebook() {
 
         contactList = new ArrayList<>();
         System.out.println("Welcome to your phonebook");
+        menuOptions.add(option1);
+        menuOptions.add(option2);
+        menuOptions.add(option3);
+        menuOptions.add(option4);
+        menuOptions.add(option5);
+        menuOptions.add(option6);
         mainMenu();
     }
 
     public void mainMenu() {
         System.out.println("Choose an option bellow:");
         System.out.println();
-        System.out.println(" 1 - Add a contact");
-        System.out.println(" 2 - Remove a contact");
-        System.out.println(" 3 - Find a contact");
-        System.out.println(" 4 - List all contacts");
-        System.out.println(" 5 - Dial a contact");
+        for (String option : menuOptions) {
+            System.out.println(option);
+        }
         System.out.println();
         System.out.print("Select by number:");
 
-        int option = numInput.nextInt();
-
-        switch (option) {
-            case 1: addContact(); break;
-            case 2: removeContact(); break;
-            case 3: findContact(); break;
-            case 4: listAll(); break;
-            case 5: dialContact(); break;
-            default: return;
+        try {
+            int option = userNumInput.nextInt();
+            switch (option) {
+                case 1: addContact(); break;
+                case 2: removeContact(); break;
+                case 3: findContact(); break;
+                case 4: listAll(); break;
+                case 5: dialContact(); break;
+                case 6: System.exit(0); break;
+                default: return;
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Please use numbers to select options");
+            userNumInput.nextLine();
         }
         mainMenu();
     }
@@ -68,7 +77,7 @@ public class Phonebook {
 
         while(true) {
             System.out.print("Contact name: ");
-            contactName = nameInput.nextLine();
+            contactName = userWordInput.nextLine();
 
             if (!(contactName.matches("([A-Za-z]{2,30})"))) {
                 System.out.println("Invalid name. Please try again");
@@ -80,12 +89,12 @@ public class Phonebook {
     }
 
     private int addContactNumber() {
-        int contactNum = 0;
+        int contactNum;
 
         while (true) {
             System.out.print("Phone number: ");
             try {
-                contactNum = numInput.nextInt();
+                contactNum = userNumInput.nextInt();
                 int contactNumLength = String.valueOf(contactNum).length();
                 if ((contactNumLength < 5) || (contactNumLength > 15)) {
                     System.out.println("Incorrect phone number length. Try again");
@@ -105,7 +114,7 @@ public class Phonebook {
 
     public void findContact() {
         System.out.println("Enter to search...");
-        String toSearch = nameInput.nextLine().toLowerCase();
+        String toSearch = userWordInput.nextLine().toLowerCase();
         ArrayList<Contact> searchResults = new ArrayList<>();
 
         for (Contact i : contactList) {
@@ -116,6 +125,11 @@ public class Phonebook {
         }
         if (searchResults.size() > 0) {
             for (Contact i : searchResults) {
+                if (searchResults.size() > 1) {
+                    System.out.println(searchResults.size() + " contacts found:");
+                } else {
+                    System.out.println("1 contact found");
+                }
                 System.out.println(i.getName() + ": " + i.getNumber());
             }
         } else {
@@ -125,6 +139,7 @@ public class Phonebook {
     }
 
     public void listAll() {
+        System.out.println("Total contacts in phonebook: " + contactList.size());
         for (Contact i : contactList) {
             System.out.println(i.getName() + ": " + i.getNumber());
         }
